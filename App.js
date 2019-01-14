f23450ef2e42a500ecce5d480e25fb55ed1f844b
Pedro Neto <pedroneto@Pedros-MacBook-Air.local>
2019-01-14 15:47:43 -0300

    auth: cadastrando usuario com email e senha

--- App.js
+++ App.js
@@ -13,12 +13,12 @@ import ListUserComponent from './ListUserComponent';
 
 export default class App extends Component {
 
-  state = {
-    users: {},
-    loading: true
-  }
+  // state = {
+  //   users: {},
+  //   loading: true
+  // }
 
-  componentWillMount() {
+  componentDidMount() {
     // Initialize Firebase
     var config = {
       apiKey: "AIzaSyAcuyPdyiPceJtEYbNr0iWH1FXyQpg_G8E",
@@ -30,47 +30,71 @@ export default class App extends Component {
     };
     firebase.initializeApp(config);
 
-    let usersRef = firebase.database().ref("usuarios");
-    usersRef.on('value', (snapshot) => {
-      let data = snapshot.val();
-      let users = Object.values(data);
-      this.setState({users: users, loading: false});
-    })
+
+    // let usersRef = firebase.database().ref("usuarios");
+    // usersRef.on('value', (snapshot) => {
+    //   let data = snapshot.val();
+    //   let users = Object.values(data);
+    //   this.setState({users: users, loading: false});
+    // })
   }
 
-  interactingFirebase() {
-    var users = firebase.database().ref("usuarios");
+  cadastrarUsuario(){
+      var email = "pedroneto@email.com";
+      var senha = "12341234";
 
-    // users.child("003").child("nome").set("Pedro Jose de Souza Neto");
-    // users.child("002").remove();
-    // users.push().child("nome").set("Pedro Jose de Souza Neto");
-    users.push().set({
-      nome: "Pedro",
-      sobrenome: "Neto"
-    });
+      const usuario = firebase.auth();
+
+      usuario.createUserWithEmailAndPassword(
+        email,
+        senha
+      ).catch(
+        (error) => {
+          // info de erros: error.code and error.message
+          alert(error.message);
+        }
+      );
   }
+
+  // interactingFirebase() {
+  //   var users = firebase.database().ref("usuarios");
+
+  //   // users.child("003").child("nome").set("Pedro Jose de Souza Neto");
+  //   // users.child("002").remove();
+  //   // users.push().child("nome").set("Pedro Jose de Souza Neto");
+  //   users.push().set({
+  //     nome: "Pedro",
+  //     sobrenome: "Neto"
+  //   });
+  // }
 
   render() {
-    if(this.state.loading){
-      return (
-        <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
-          <ActivityIndicator size="large" color="dodgerblue" />
-        </View>
-      )
-    }
-    else{
-      return (
-        <View style={styles.container}>
-          <Button onPress={ () => this.interactingFirebase() } title="New Interaction" color="#841584" accessibilityLabel="Salvar Dados" />
+    return(
+      <View style={styles.container}>
+        <Button onPress={ () => this.cadastrarUsuario() } title="New User" color="#841584" accessibilityLabel="New User" />
+      </View>
+    )
+    
+    // if(this.state.loading){
+    //   return (
+    //     <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
+    //       <ActivityIndicator size="large" color="dodgerblue" />
+    //     </View>
+    //   )
+    // }
+    // else{
+    //   return (
+    //     <View style={styles.container}>
+    //       <Button onPress={ () => this.interactingFirebase() } title="New Interaction" color="#841584" accessibilityLabel="Salvar Dados" />
   
-          {
-            this.state.users.length > 0
-            ? <ListUserComponent users={this.state.users} />
-            : <Text>No Users</Text>
-          }
-        </View>
-      );
-    }
+    //       {
+    //         this.state.users.length > 0
+    //         ? <ListUserComponent users={this.state.users} />
+    //         : <Text>No Users</Text>
+    //       }
+    //     </View>
+    //   );
+    // }
   }
 }
 
